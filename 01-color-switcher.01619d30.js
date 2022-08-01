a12bf1d1f46bee00f979eea7e69d67f9f853958a
Prime-DS <prime.up17@gmail.com>
2022-08-01 18:43:25 +0000

    Deploying to gh-pages from @ Prime-DS/goit-js-hw-09@c228ae19f94ec79482987db01f68b8b021bc848f 🚀

---
+++ 01-color-switcher.01619d30.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.querySelector("body");let n,r=!0;t.addEventListener("click",(function(){r&&(r=!1,n=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3))})),e.addEventListener("click",(function(){r=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.01619d30.js.map
---
+++ 01-color-switcher.3f1d9a37.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),o=document.querySelector("body"),c=!0;e.addEventListener("click",(function(){c&&(c=!1,t=setInterval((function(){o.style.backgroundColor="".concat("#".concat(Math.floor(16777215*Math.random()).toString(16)))}),1e3))})),n.addEventListener("click",(function(){c=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.3f1d9a37.js.map
